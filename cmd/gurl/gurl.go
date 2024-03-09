6a40cee4c6589b207425cbbd51cf9da83839c83d
soypat <graded.sp@gmail.com>
2024-03-09 16:46:39 -0300

    add gurl: curl but in pure go

---
+++ cmd/gurl/gurl.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"log/slog"
+	"net"
+	"net/netip"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/soypat/netif"
+	"github.com/soypat/netif/examples/common"
+	"github.com/soypat/seqs"
+	"github.com/soypat/seqs/eth/dns"
+	"github.com/soypat/seqs/httpx"
+	"github.com/soypat/seqs/stacks"
+)
+
+const connTimeout = 5 * time.Second
+
+const ourHost = "gurl"
+const dnsTimeout = 4 * time.Second
+
+func main() {
+	var (
+		flagInterface string
+		serverPort    uint16
+		flagLogLevel  int
+	)
+
+	// Create device interface.
+	iface, err := netif.DefaultInterface()
+	if err != nil {
+		iface, err = net.InterfaceByIndex(1)
+		if err != nil {
+			log.Fatal("no interfaces found:", err)
+		}
+	}
+	flag.StringVar(&flagInterface, "i", iface.Name, "Interface to use")
+	flag.IntVar(&flagLogLevel, "l", int(slog.LevelInfo), "Log level")
+	flag.Parse()
+	if flag.NArg() > 1 {
+		log.Fatal("too many arguments")
+	}
+
+	// Parse URL and validate it.
+	argURL := flag.Arg(0)
+	if argURL == "" {
+		log.Fatal("URL is required")
+	}
+	URL, err := url.Parse(argURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	svHostname := URL.Host
+	if newhost, sport, ok := strings.Cut(svHostname, ":"); ok {
+		svHostname = newhost
+		p, err := strconv.Atoi(sport)
+		if err != nil || p < 1 || p > 65535 {
+			log.Fatal("invalid port w/ parse err:", err)
+		}
+		serverPort = uint16(p)
+	}
+
+	// Create structured logger.
+	fp, _ := os.Create("http-client.log")
+	logger := slog.New(slog.NewTextHandler(io.MultiWriter(fp, os.Stdout), &slog.HandlerOptions{
+		Level: slog.Level(flagLogLevel),
+	}))
+
+	slog.Info("url", slog.String("url", URL.String()), slog.String("uri", URL.RequestURI()), slog.String("host", svHostname), slog.Uint64("port", uint64(serverPort)))
+
+	// Check whether we need to resolve hostname.
+	_, dnsErr := dns.NewName(svHostname)
+	serverAddr, ipErr := netip.ParseAddr(svHostname)
+	if dnsErr != nil && ipErr != nil {
+		log.Fatal("invalid hostname", dnsErr, ipErr)
+	}
+
+	// OK, all pre-processing is done, lets open the socket and start the client.
+	// This performs a DHCP setup to acquire network data.
+	ethsock, err := netif.NewEthSocket(flagInterface)
+	if err != nil {
+		log.Fatal("ethernet socket:" + err.Error())
+	}
+	dhcpc, stack, err := common.SetupWithDHCP(ethsock, common.SetupConfig{
+		Hostname: ourHost,
+		Logger:   logger,
+		TCPPorts: 1, // For HTTP over TCP.
+		UDPPorts: 1, // For DNS.
+	})
+	if err != nil {
+		panic("setup DHCP:" + err.Error())
+	}
+
+	if ipErr != nil {
+		// We have a hostname we must resolve.
+		resolver, err := common.NewResolver(stack, dhcpc)
+		if err != nil {
+			panic("resolver create:" + err.Error())
+		}
+		addrs, err := resolver.LookupNetIP(URL.Host)
+		if err != nil {
+			panic("DNS lookup failed:" + err.Error())
+		}
+		serverAddr = addrs[0]
+	}
+
+	// Next resolve the router's hardware address.
+	routerhw, err := common.ResolveHardwareAddr(stack, dhcpc.Router(), dnsTimeout)
+	if err != nil {
+		panic("router hwaddr resolving:" + err.Error())
+	}
+
+	// Start TCP server.
+	const clientPort = 80
+	clientAddr := netip.AddrPortFrom(stack.Addr(), clientPort)
+	conn, err := stacks.NewTCPConn(stack, stacks.TCPConnConfig{
+		TxBufSize: tcpBufSize(iface.MTU),
+		RxBufSize: tcpBufSize(iface.MTU),
+	})
+	if err != nil {
+		panic("conn create:" + err.Error())
+	}
+
+	closeConn := func(err string) {
+		slog.Error("tcpconn:closing", slog.String("err", err))
+		conn.Close()
+		for !conn.State().IsClosed() {
+			slog.Info("tcpconn:waiting", slog.String("state", conn.State().String()))
+			time.Sleep(1000 * time.Millisecond)
+		}
+	}
+
+	var req httpx.RequestHeader
+	req.SetRequestURI(URL.RequestURI())
+	req.SetMethod("GET")
+	reqbytes := req.Header()
+
+	logger.Info("tcp:ready",
+		slog.String("clientaddr", clientAddr.String()),
+		slog.String("serveraddr", serverAddr.String()),
+	)
+	rxBuf := make([]byte, iface.MTU*8)
+	for {
+		time.Sleep(5 * time.Second)
+		slog.Info("dialing", slog.String("serveraddr", serverAddr.String()))
+
+		// Make sure to timeout the connection if it takes too long.
+		conn.SetDeadline(time.Now().Add(connTimeout))
+
+		err = conn.OpenDialTCP(clientAddr.Port(), routerhw, netip.AddrPortFrom(serverAddr, serverPort), 0x123456)
+		if err != nil {
+			closeConn("opening TCP: " + err.Error())
+			continue
+		}
+		retries := 50
+		for conn.State() != seqs.StateEstablished && retries > 0 {
+			time.Sleep(100 * time.Millisecond)
+			retries--
+		}
+		conn.SetDeadline(time.Time{}) // Disable the deadline.
+		if retries == 0 {
+			closeConn("tcp establish retry limit exceeded")
+			continue
+		}
+
+		// Send the request.
+		_, err = conn.Write(reqbytes)
+		if err != nil {
+			closeConn("writing request: " + err.Error())
+			continue
+		}
+		time.Sleep(500 * time.Millisecond)
+		conn.SetDeadline(time.Now().Add(connTimeout))
+		n, err := conn.Read(rxBuf)
+		if n == 0 && err != nil {
+			closeConn("reading response: " + err.Error())
+			continue
+		} else if n == 0 {
+			closeConn("no response")
+			continue
+		}
+		logger.Info("response", slog.String("response", string(rxBuf[:n])))
+		closeConn("done")
+	}
+
+}
+
+func tcpBufSize(mtu int) uint16 {
+	return uint16(mtu - 40)
+}
