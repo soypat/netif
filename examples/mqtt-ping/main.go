ffa71927ff88fbfbe0f8ebbbd4273836e4239e7e
soypat <graded.sp@gmail.com>
2024-03-15 01:15:36 -0300

    fix mqtt example and bump seqs version

--- examples/mqtt-ping/main.go
+++ examples/mqtt-ping/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"log/slog"
+	"math/rand"
 	"net"
 	"net/netip"
 	"os"
@@ -15,6 +16,7 @@ import (
 	"github.com/soypat/netif"
 	"github.com/soypat/netif/examples/common"
 	"github.com/soypat/seqs"
+	"github.com/soypat/seqs/eth/dns"
 	"github.com/soypat/seqs/stacks"
 )
 
@@ -39,7 +41,7 @@ var (
 
 func main() {
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelDebug - 4,
+		Level: slog.LevelDebug - 1,
 	})))
 	u, _ := user.Current()
 	if u.Name == "" {
@@ -56,16 +58,20 @@ func main() {
 		flagNetInterfaceName = iface.Name
 		flagServerHostname   = "test.mosquitto.org"
 		flagServerPort       = 1883
-		flagOurPort          = 1883
+		flagOurPort          = 0
 		flagMQTTTopic        = "test"
 		flagClientID         = u.Name
+		flagPassword         = ""
+		flagRequestedIP      string
 	)
 	flag.StringVar(&flagNetInterfaceName, "i", flagNetInterfaceName, "Network interface name. e.g. eth0, wlan0, wlp7s0, enp044s etc.")
+	flag.StringVar(&flagRequestedIP, "d", "", "IP address to request by DHCP, or assigned if DHCP unavailable")
 	flag.StringVar(&flagServerHostname, "server", flagServerHostname, "MQTT server hostname")
 	flag.IntVar(&flagServerPort, "port", flagServerPort, "MQTT server TCP port")
 	flag.IntVar(&flagOurPort, "ourport", flagOurPort, "Our MQTT TCP port")
 	flag.StringVar(&flagMQTTTopic, "topic", flagMQTTTopic, "MQTT topic to subscribe to")
 	flag.StringVar(&flagClientID, "clientid", flagClientID, "MQTT client ID")
+	flag.StringVar(&flagPassword, "password", flagPassword, "MQTT password")
 	flag.Parse()
 	dev, err := netif.NewEthSocket(flagNetInterfaceName)
 	if err != nil {
@@ -74,28 +80,48 @@ func main() {
 	defer dev.Close()
 
 	dhcpc, stack, err := common.SetupWithDHCP(dev, common.SetupConfig{
-		Hostname: "myhost",
-		Logger:   slog.Default(),
-		UDPPorts: 1, // DNS
-		TCPPorts: 1, // TCP stuff.
+		Hostname:    "mqtt-pinger",
+		Logger:      slog.Default(),
+		UDPPorts:    1, // DNS
+		TCPPorts:    1, // TCP stuff.
+		DHCPTimeout: time.Second,
+		RequestedIP: flagRequestedIP,
 	})
 	if err != nil {
 		log.Fatal("SetupWithDHCP:", err)
 	}
-	// stack.RecvEth()
-	routerhw, err := common.ResolveHardwareAddr(stack, dhcpc.Router(), 5*time.Second)
-	if err != nil {
-		panic("router hwaddr resolving:" + err.Error())
+
+	// Determine Server IP address and MAC address of server (or router if DNS is used).
+	_, domainErr := dns.NewName(flagServerHostname)
+	serverAddr, ipErr := netip.ParseAddr(flagServerHostname)
+	if domainErr != nil && ipErr != nil {
+		log.Fatal("invalid hostname", flagServerHostname)
 	}
-	resolver, err := common.NewResolver(stack, dhcpc)
-	if err != nil {
-		panic("resolver create:" + err.Error())
+	isDomain := domainErr == nil && ipErr != nil
+	var hwAddr [6]byte
+	if isDomain {
+		slog.Info("resolving via DNS")
+		// Is a domain we must resolve via DNS.
+		hwAddr, err = common.ResolveHardwareAddr(stack, dhcpc.Router(), 5*time.Second)
+		if err != nil {
+			log.Fatal("router hwaddr resolving:", err.Error())
+		}
+		resolver, err := common.NewResolver(stack, dhcpc)
+		if err != nil {
+			log.Fatal("resolver create:", err.Error())
+		}
+		addrs, err := resolver.LookupNetIP(flagServerHostname)
+		if err != nil {
+			log.Fatal("DNS lookup failed:", err.Error())
+		}
+		serverAddr = addrs[0]
+	} else {
+		slog.Info("resolving using only ARP")
+		hwAddr, err = common.ResolveHardwareAddr(stack, serverAddr, 5*time.Second)
+		if err != nil {
+			log.Fatal("router hwaddr resolving:", err.Error())
+		}
 	}
-	addrs, err := resolver.LookupNetIP(flagServerHostname)
-	if err != nil {
-		panic("DNS lookup failed:" + err.Error())
-	}
-	serverAddr := netip.AddrPortFrom(addrs[0], uint16(flagServerPort))
 
 	// Start TCP server.
 	const socketBuf = 16 * 1024
@@ -105,8 +131,11 @@ func main() {
 	}
 	var varconn mqtt.VariablesConnect
 	varconn.SetDefaultMQTT([]byte(flagClientID))
-	client := mqtt.NewClient(mqttCfg)
+	if flagPassword != "" {
+		varconn.Password = []byte(flagPassword)
+	}
 
+	client := mqtt.NewClient(mqttCfg)
 	closeConn := func() {
 		slog.Info("socket:close-connection", slog.String("state", socket.State().String()))
 		socket.FlushOutputBuffer()
@@ -114,12 +143,19 @@ func main() {
 		for !socket.State().IsClosed() {
 			time.Sleep(100 * time.Millisecond)
 		}
+		time.Sleep(time.Second)
 	}
 
 	// Connection loop for TCP+MQTT.
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for {
 		slog.Info("socket:listen")
-		err = socket.OpenDialTCP(uint16(flagOurPort), routerhw, serverAddr, 0x123456)
+		ourPort := uint16(flagOurPort)
+		if ourPort == 0 {
+			// If flag not set generate random port.
+			ourPort = uint16(rng.Intn(0xffff-1024) + 1024)
+		}
+		err = socket.OpenDialTCP(ourPort, hwAddr, netip.AddrPortFrom(serverAddr, uint16(flagServerPort)), seqs.Value(rng.Intn(0xffff_ffff))+1)
 		if err != nil {
 			panic("socket dial:" + err.Error())
 		}
@@ -145,6 +181,11 @@ func main() {
 		}
 		retries = 50
 		for retries > 0 && !client.IsConnected() {
+			err = client.HandleNext()
+			if err != nil {
+				slog.Error("mqtt:handle-next-failed", slog.String("reason", err.Error()))
+				closeConn()
+			}
 			time.Sleep(100 * time.Millisecond)
 			retries--
 		}
@@ -154,18 +195,31 @@ func main() {
 			continue
 		}
 
-		for client.IsConnected() {
-			socket.SetDeadline(time.Now().Add(5 * time.Second))
-			pubVar.PacketIdentifier++
-			err = client.PublishPayload(pubFlags, pubVar, []byte("hello world"))
-			if err != nil {
-				slog.Error("mqtt:publish-failed", slog.Any("reason", err))
-			}
-			slog.Info("published message", slog.Uint64("packetID", uint64(pubVar.PacketIdentifier)))
-			time.Sleep(5 * time.Second)
+		err = client.StartPing()
+		if err != nil {
+			slog.Error("mqtt:ping-failed", slog.Any("reason", err))
+			closeConn()
+			continue
 		}
-		slog.Error("mqtt:disconnected", slog.Any("reason", client.Err()))
+		start := client.LastTx()
+		for client.AwaitingPingresp() {
+			if socket.BufferedInput() <= 0 {
+				slog.Info("mqtt:wait-for-response")
+				time.Sleep(time.Second)
+				continue
+			}
+			err = client.HandleNext()
+			if err != nil {
+				slog.Error("mqtt:ping-failed", slog.Any("reason", err))
+				closeConn()
+				break
+			}
+			time.Sleep(2 * time.Millisecond)
+		}
+		end := client.LastRx()
+		slog.Info("mqtt:ping", slog.Duration("ping-time", end.Sub(start)))
 		closeConn()
+		break
 	}
 }
 
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.20
 
 require (
 	github.com/soypat/natiu-mqtt v0.5.1
-	github.com/soypat/seqs v0.0.0-20240308030925-3034cafd7839
+	github.com/soypat/seqs v0.0.0-20240315041335-3dd1de606d2f
 	golang.org/x/sys v0.17.0
 )
